fd816ea413e2f26ffeaf82b19ed82f455f6e1fc9
Andy Shu Xin <andyshuxin@gmail.com>
2015-08-17 19:40:20 +0800

    finish question page

--- src/scripts/data_hant.js
+++ src/scripts/data_hant.js
@@ -14,7 +14,7 @@ wyQuiz.survey = [
   {
     "serial": 0,
     "questionText": "政府派錢六千元，我會立即search廉價機票，計劃去旅行",
-    "containsImage": false,
+    "imagePath": "images/optionImage1.png",
     "options": [
       {"optionScore": 3, "optionText":"很同意"},
       {"optionScore": 2, "optionText":"較同意"},
@@ -25,7 +25,6 @@ wyQuiz.survey = [
   {
     "serial": 1,
     "questionText": "就算老闆加錢要求我OT，我都寧願選擇去friend的party",
-    "containsImage": false,
     "options": [
       {"optionScore": 3, "optionText":"很同意"},
       {"optionScore": 2, "optionText":"較同意"},
@@ -36,7 +35,7 @@ wyQuiz.survey = [
   {
     "serial": 2,
     "questionText": "每當要shopping，我就會立即想到上網淘寶",
-    "containsImage": false,
+    "imagePath": "images/optionImage1.png",
     "options": [
       {"optionScore": 3, "optionText":"很同意"},
       {"optionScore": 2, "optionText":"較同意"},
@@ -47,7 +46,6 @@ wyQuiz.survey = [
   {
     "serial": 3,
     "questionText": "我覺得我的性別是",
-    "containsImage": false,
     "options": [
       {"optionScore": 0, "optionText":"男"},
       {"optionScore": 0, "optionText":"女"},
@@ -58,7 +56,6 @@ wyQuiz.survey = [
   {
     "serial": 4,
     "questionText": "我覺得自己一定可以賺夠錢買樓，等政府派公屋的都是Loser",
-    "containsImage": false,
     "options": [
       {"optionScore": 0, "optionText":"很同意"},
       {"optionScore": 1, "optionText":"較同意"},
@@ -69,7 +66,6 @@ wyQuiz.survey = [
   {
     "serial": 5,
     "questionText": "如果有得揀，我會想做李嘉誠，而不是王維基",
-    "containsImage": false,
     "options": [
       {"optionScore": 0, "optionText":"很同意"},
       {"optionScore": 1, "optionText":"較同意"},
@@ -79,8 +75,7 @@ wyQuiz.survey = [
   },
   {
     "serial": 6,
-    "questionText": "如果有得揀，我會想做李嘉誠，而不是王維基",
-    "containsImage": false,
+    "questionText": "我認爲社會上大多數的成功青年其實都是「靠父幹」",
     "options": [
       {"optionScore": 3, "optionText":"很同意"},
       {"optionScore": 2, "optionText":"較同意"},
@@ -91,7 +86,6 @@ wyQuiz.survey = [
   {
     "serial": 7,
     "questionText": "講真，我的歲數是",
-    "containsImage": false,
     "options": [
       {"optionScore": 0, "optionText":"18歲以下"},
       {"optionScore": 0, "optionText":"18-25歲"},
@@ -102,7 +96,6 @@ wyQuiz.survey = [
   {
     "serial": 8,
     "questionText": "當父母批評我班friend好hea或好頹，我會減少同這班朋友來往",
-    "containsImage": false,
     "options": [
       {"optionScore": 0, "optionText":"很同意"},
       {"optionScore": 1, "optionText":"較同意"},
@@ -113,7 +106,6 @@ wyQuiz.survey = [
   {
     "serial": 9,
     "questionText": "無論其他人怎樣評價我，都不會改變我對自己的認知",
-    "containsImage": false,
     "options": [
       {"optionScore": 3, "optionText":"很同意"},
       {"optionScore": 2, "optionText":"較同意"},
@@ -124,7 +116,6 @@ wyQuiz.survey = [
   {
     "serial": 10,
     "questionText": "特首選舉，怎麼都要有公民提名",
-    "containsImage": false,
     "options": [
       {"optionScore": 3, "optionText":"很同意"},
       {"optionScore": 2, "optionText":"較同意"},
@@ -135,7 +126,6 @@ wyQuiz.survey = [
   {
     "serial": 11,
     "questionText": "其實我讀書讀到",
-    "containsImage": false,
     "options": [
       {"optionScore": 0, "optionText":"小學畢業"},
       {"optionScore": 0, "optionText":"中學畢業"},
@@ -146,7 +136,6 @@ wyQuiz.survey = [
   {
     "serial": 12,
     "questionText": "同父母吵架，我一定會揸緊宗旨，堅持立場",
-    "containsImage": false,
     "options": [
       {"optionScore": 0, "optionText":"很同意"},
       {"optionScore": 1, "optionText":"較同意"},
@@ -157,7 +146,6 @@ wyQuiz.survey = [
   {
     "serial": 13,
     "questionText": "要爭取社會公義，我覺得上街遊行、集會，比辯論更直接有效",
-    "containsImage": false,
     "options": [
       {"optionScore": 3, "optionText":"很同意"},
       {"optionScore": 2, "optionText":"較同意"},
@@ -168,7 +156,6 @@ wyQuiz.survey = [
   {
     "serial": 14,
     "questionText": "比起報紙、雜誌，我更喜歡從網絡獲取資訊",
-    "containsImage": false,
     "options": [
       {"optionScore": 3, "optionText":"很同意"},
       {"optionScore": 2, "optionText":"較同意"},
@@ -179,7 +166,6 @@ wyQuiz.survey = [
   {
     "serial": 15,
     "questionText": "有人說過我是廢青",
-    "containsImage": false,
     "options": [
       {"optionScore": 0, "optionText":"是"},
       {"optionScore": 0, "optionText":"否"},
@@ -188,7 +174,6 @@ wyQuiz.survey = [
   {
     "serial": 16,
     "questionText": "情到濃時，我覺得在公眾地方進行性行為是可以接受的",
-    "containsImage": false,
     "options": [
       {"optionScore": 3, "optionText":"很同意"},
       {"optionScore": 2, "optionText":"較同意"},
@@ -199,7 +184,6 @@ wyQuiz.survey = [
   {
     "serial": 17,
     "questionText": "我覺得援交、伴遊，與做侍應一樣，只是賺錢的兼職",
-    "containsImage": false,
     "options": [
       {"optionScore": 3, "optionText":"很同意"},
       {"optionScore": 2, "optionText":"較同意"},
@@ -210,7 +194,6 @@ wyQuiz.survey = [
   {
     "serial": 18,
     "questionText": "除了男女關係，我認為男男或女女，都不是正常戀愛",
-    "containsImage": false,
     "options": [
       {"optionScore": 0, "optionText":"很同意"},
       {"optionScore": 1, "optionText":"較同意"},
@@ -221,7 +204,6 @@ wyQuiz.survey = [
   {
     "serial": 19,
     "questionText": "有人說過我是廢青",
-    "containsImage": false,
     "options": [
       {"optionScore": 0, "optionText":"是"},
       {"optionScore": 0, "optionText":"否"},
--- src/scripts/main.jsx
+++ src/scripts/main.jsx
@@ -15,6 +15,8 @@ wyQuiz.setNewUUID = function() {
   } else {
     var url = 'http://s.init.im:8081/utility/uuid/';
     var uuid = 'DEFAULT'+Math.random().toString(); // In case UUID server fails
+    this.uuid = uuid;
+    localStorage.setItem('uuid', uuid);
     var request = new XMLHttpRequest();
 
     request.open('GET', url, true);
@@ -70,17 +72,177 @@ function extend(object1, object2) {
   return result;
 }
 
+var themeColor = "#2AB6C9";
+
 var masterStyle = {
-  backgroundColor: '#2AB6C9',
+  backgroundColor: themeColor,
   fontFamily: 'sans-serif',
   maxWidth: '1125px',
-  margin: '0 auto'
+  margin: '0 auto',
+  color: '#EEE',
+  fontSize: '2em',
+  padding: '1em'
 };
 
-var QuestionCard = React.createClass({
+var ResultPage = React.createClass({
   render: function() {
+    return (null);
+    //TODO
+  }
+});
+
+var QuestionCard = React.createClass({
+
+  getInitialState: function() {
+    return {
+      questionSerial: this.props.questionSerial,
+      totalScore: 0
+    }
+  },
+
+  handleOptionClick: function(event) {
+
+    console.log(this.state.questionSerial);
+    console.log(this.props.survey.length);
+
+    if (this.state.questionSerial < this.props.survey.length - 1) {
+      this.setState({
+        questionSerial: this.state.questionSerial + 1,
+        totalScore: this.state.totalScore + event.target.getAttribute("data-score")
+      });
+    } else {
+      // Last Question
+      this.setState({
+        totalScore: this.state.totalScore + event.target.getAttribute("data-score")
+      });
+      React.render(
+          <ResultPage totalScore={this.state.totalScore} />,
+          document.getElementById('content')
+      )
+    }
+
+  },
+
+  render: function() {
+
+    var question = this.props.survey[this.state.questionSerial];
+
+    // === Styles ===
+    var cardStyle = {
+      position: "relative"
+    };
+    cardStyle = extend(masterStyle, cardStyle);
+
+    var listItemStyle = {
+      marginTop: "0.5em",
+      cursor: "pointer",
+      maxWidth: "5em",
+    };
+
+    var imageStyle = {
+      position: "absolute",
+      bottom: 0,
+      right: 0,
+      maxHeight: "8em",
+    };
+
+    var progressBarBoxStyle = {
+      position: "relative",
+      overflow: "hidden",
+      whiteSpace: "nowrap",
+    };
+
+    var progressBarStyle = {
+      border: "2px solid white",
+      height: "1em",
+      display: 'inline-block',
+      width: '95%',
+      textAlign: "center",
+    };
+
+    var questionTextStyle = {
+      fontSize: "1.5em"
+    };
+
+    var optionBoxStyle = {
+      overflow: "hidden",
+      position: "relative"
+    };
+
+    var progressPercentage = (this.state.questionSerial / this.props.survey.length);
+    var finishedBarStyle = {
+      position: "relative",
+      height: "1em",
+      backgroundColor: "#EEE",
+      width: (5 + (95 - 5) * progressPercentage).toString() + '%',
+    };
+
+    var finishedQuestionCountStyle = {
+      float: "right",
+      paddingBottom: "0.1em",
+      paddingRight: "0.2em",
+      color: themeColor,
+    };
+
+    var totalQuestionCountStyle = {
+      float: "right",
+      width: "1.5em"
+    };
+    // === End of Styles ===
+
+    // Generate list items
+    var optionListItems = question.options.map(function(option){
+      return (
+          <li key={option.optionText}
+              onClick={this.handleOptionClick}
+              data-score={option.optionScore}
+              style={listItemStyle}
+          >
+            {option.optionText}
+          </li>
+      );
+    }, this);
+
     return (
-        <div>Hello</div>
+        <div id="QuestionCard" style={cardStyle}>
+          <div id="QuestionText"
+               style={questionTextStyle}
+          >
+            {question.questionText}
+          </div>
+          <div id="OptionBox"
+              style={optionBoxStyle}>
+            <ul id="OptionList">
+              {optionListItems}
+            </ul>
+            {question.imagePath ?
+              <img id="OptionImage"
+                   src={question.imagePath}
+                   style={imageStyle}
+                  /> :
+              <div></div>
+            }
+          </div>
+
+          <div id="ProgressBarBox"
+               style={progressBarBoxStyle}>
+            <div id="ProgressBar"
+                 style={progressBarStyle}>
+              <div id="divFinishedBar"
+                   style={finishedBarStyle}>
+                <div id="spanFinishedQuestionCount"
+                      style={finishedQuestionCountStyle}>
+                  {this.state.questionSerial}
+                </div>
+              </div>
+            </div>
+
+            <span id="spanTotalQuestionCount"
+                  style={totalQuestionCountStyle}>
+              {this.props.survey.length}
+            </span>
+          </div>
+        </div>
     );
   }
 });
@@ -88,11 +250,10 @@ var QuestionCard = React.createClass({
 var CoverCard = React.createClass({
 
   handleStartClick: function(event) {
-    target.preventDefault();
     React.render(
         <QuestionCard
             survey={wyQuiz.survey}
-            serial={0}
+            questionSerial={0}
         />,
         document.getElementById('content')
     )
@@ -104,7 +265,6 @@ var CoverCard = React.createClass({
     cardStyle = extend(masterStyle, cardStyle);
 
     var textBoxStyle = {
-      color: '#EEE',
     };
 
     var imageStyle = {
@@ -121,7 +281,8 @@ var CoverCard = React.createClass({
       fontColor: 'inherit',
       fontSize: '2em',
       textDecoration: 'none',
-      marginLeft: '15em'
+      marginLeft: '15em',
+      cursor: 'pointer',
     };
 
 
@@ -130,11 +291,11 @@ var CoverCard = React.createClass({
           <img src={this.props.coverImagePath} style={imageStyle} />
           <div id='textBox' style={textBoxStyle}>
             <h1 style={titleStyle}>{this.props.title}</h1>
-            <a href=''
+            <span
                style={startTextStyle}
                onClick={this.handleStartClick}>
               {this.props.startText + "▶"}
-            </a>
+            </span>
           </div>
         </div>
     )
@@ -145,15 +306,14 @@ var CoverCard = React.createClass({
   document.title = wyQuiz.title;
   wyQuiz.setNewUUID();
   React.render(
-      <CoverCard
-          title={wyQuiz.title}
-          startText={wyQuiz.startText}
-          coverImagePath={wyQuiz.coverImgRelativePath}
-      />,
+      //<CoverCard
+      //    title={wyQuiz.title}
+      //    startText={wyQuiz.startText}
+      //    coverImagePath={wyQuiz.coverImgRelativePath}
+      ///>,
+      <QuestionCard survey={wyQuiz.survey} questionSerial={0} />,
       document.getElementById('content')
   );
 
   post('render', wyQuiz.lang+'-rendered');
 }(window, window.document, window.React, window.wyQuiz));
-
-
