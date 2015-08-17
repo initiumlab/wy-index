81577b854a875f6323bc4c5dcb15d7ec22404b72
Andy Shu Xin <andyshuxin@gmail.com>
2015-08-17 16:40:22 +0800

    finish cover page

---
+++ src/scripts/data_hant.js
@@ -0,0 +1,231 @@
+var wyQuiz = {
+  codeName: "wy-Index",
+  title: "廢青指數測驗",
+  startText: "開始",
+  url: 'http://dprktest.initiumlab.com/',
+  bannerImgRelativePath: './images/banner_hant.png',
+  coverImgRelativePath: './images/cover.png',
+  lang: 'zh-hant',
+
+  totalScore: 0
+};
+
+wyQuiz.survey = [
+  {
+    "serial": 0,
+    "questionText": "政府派錢六千元，我會立即search廉價機票，計劃去旅行",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 3, "optionText":"很同意"},
+      {"optionScore": 2, "optionText":"較同意"},
+      {"optionScore": 1, "optionText":"較反對"},
+      {"optionScore": 0, "optionText":"很反對"},
+    ]
+  },
+  {
+    "serial": 1,
+    "questionText": "就算老闆加錢要求我OT，我都寧願選擇去friend的party",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 3, "optionText":"很同意"},
+      {"optionScore": 2, "optionText":"較同意"},
+      {"optionScore": 1, "optionText":"較反對"},
+      {"optionScore": 0, "optionText":"很反對"},
+    ]
+  },
+  {
+    "serial": 2,
+    "questionText": "每當要shopping，我就會立即想到上網淘寶",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 3, "optionText":"很同意"},
+      {"optionScore": 2, "optionText":"較同意"},
+      {"optionScore": 1, "optionText":"較反對"},
+      {"optionScore": 0, "optionText":"很反對"},
+    ]
+  },
+  {
+    "serial": 3,
+    "questionText": "我覺得我的性別是",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 0, "optionText":"男"},
+      {"optionScore": 0, "optionText":"女"},
+      {"optionScore": 0, "optionText":"皆非"},
+      {"optionScore": 0, "optionText":"不確定"},
+    ]
+  },
+  {
+    "serial": 4,
+    "questionText": "我覺得自己一定可以賺夠錢買樓，等政府派公屋的都是Loser",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 0, "optionText":"很同意"},
+      {"optionScore": 1, "optionText":"較同意"},
+      {"optionScore": 2, "optionText":"較反對"},
+      {"optionScore": 3, "optionText":"很反對"},
+    ]
+  },
+  {
+    "serial": 5,
+    "questionText": "如果有得揀，我會想做李嘉誠，而不是王維基",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 0, "optionText":"很同意"},
+      {"optionScore": 1, "optionText":"較同意"},
+      {"optionScore": 2, "optionText":"較反對"},
+      {"optionScore": 3, "optionText":"很反對"},
+    ]
+  },
+  {
+    "serial": 6,
+    "questionText": "如果有得揀，我會想做李嘉誠，而不是王維基",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 3, "optionText":"很同意"},
+      {"optionScore": 2, "optionText":"較同意"},
+      {"optionScore": 1, "optionText":"較反對"},
+      {"optionScore": 0, "optionText":"很反對"},
+    ]
+  },
+  {
+    "serial": 7,
+    "questionText": "講真，我的歲數是",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 0, "optionText":"18歲以下"},
+      {"optionScore": 0, "optionText":"18-25歲"},
+      {"optionScore": 0, "optionText":"26-30歲"},
+      {"optionScore": 0, "optionText":"30歲以上"},
+    ]
+  },
+  {
+    "serial": 8,
+    "questionText": "當父母批評我班friend好hea或好頹，我會減少同這班朋友來往",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 0, "optionText":"很同意"},
+      {"optionScore": 1, "optionText":"較同意"},
+      {"optionScore": 2, "optionText":"較反對"},
+      {"optionScore": 3, "optionText":"很反對"},
+    ]
+  },
+  {
+    "serial": 9,
+    "questionText": "無論其他人怎樣評價我，都不會改變我對自己的認知",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 3, "optionText":"很同意"},
+      {"optionScore": 2, "optionText":"較同意"},
+      {"optionScore": 1, "optionText":"較反對"},
+      {"optionScore": 0, "optionText":"很反對"},
+    ]
+  },
+  {
+    "serial": 10,
+    "questionText": "特首選舉，怎麼都要有公民提名",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 3, "optionText":"很同意"},
+      {"optionScore": 2, "optionText":"較同意"},
+      {"optionScore": 1, "optionText":"較反對"},
+      {"optionScore": 0, "optionText":"很反對"},
+    ]
+  },
+  {
+    "serial": 11,
+    "questionText": "其實我讀書讀到",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 0, "optionText":"小學畢業"},
+      {"optionScore": 0, "optionText":"中學畢業"},
+      {"optionScore": 0, "optionText":"大專畢業"},
+      {"optionScore": 0, "optionText":"大學或以上"},
+    ]
+  },
+  {
+    "serial": 12,
+    "questionText": "同父母吵架，我一定會揸緊宗旨，堅持立場",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 0, "optionText":"很同意"},
+      {"optionScore": 1, "optionText":"較同意"},
+      {"optionScore": 2, "optionText":"較反對"},
+      {"optionScore": 3, "optionText":"很反對"},
+    ]
+  },
+  {
+    "serial": 13,
+    "questionText": "要爭取社會公義，我覺得上街遊行、集會，比辯論更直接有效",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 3, "optionText":"很同意"},
+      {"optionScore": 2, "optionText":"較同意"},
+      {"optionScore": 1, "optionText":"較反對"},
+      {"optionScore": 0, "optionText":"很反對"},
+    ]
+  },
+  {
+    "serial": 14,
+    "questionText": "比起報紙、雜誌，我更喜歡從網絡獲取資訊",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 3, "optionText":"很同意"},
+      {"optionScore": 2, "optionText":"較同意"},
+      {"optionScore": 1, "optionText":"較反對"},
+      {"optionScore": 0, "optionText":"很反對"},
+    ]
+  },
+  {
+    "serial": 15,
+    "questionText": "有人說過我是廢青",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 0, "optionText":"是"},
+      {"optionScore": 0, "optionText":"否"},
+    ]
+  },
+  {
+    "serial": 16,
+    "questionText": "情到濃時，我覺得在公眾地方進行性行為是可以接受的",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 3, "optionText":"很同意"},
+      {"optionScore": 2, "optionText":"較同意"},
+      {"optionScore": 1, "optionText":"較反對"},
+      {"optionScore": 0, "optionText":"很反對"},
+    ]
+  },
+  {
+    "serial": 17,
+    "questionText": "我覺得援交、伴遊，與做侍應一樣，只是賺錢的兼職",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 3, "optionText":"很同意"},
+      {"optionScore": 2, "optionText":"較同意"},
+      {"optionScore": 1, "optionText":"較反對"},
+      {"optionScore": 0, "optionText":"很反對"},
+    ]
+  },
+  {
+    "serial": 18,
+    "questionText": "除了男女關係，我認為男男或女女，都不是正常戀愛",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 0, "optionText":"很同意"},
+      {"optionScore": 1, "optionText":"較同意"},
+      {"optionScore": 2, "optionText":"較反對"},
+      {"optionScore": 3, "optionText":"很反對"},
+    ]
+  },
+  {
+    "serial": 19,
+    "questionText": "有人說過我是廢青",
+    "containsImage": false,
+    "options": [
+      {"optionScore": 0, "optionText":"是"},
+      {"optionScore": 0, "optionText":"否"},
+    ]
+  },
+];
+
--- src/scripts/main.jsx
+++ src/scripts/main.jsx
@@ -1,6 +1,159 @@
-var React = require('react');
+xdomain.slaves({
+  "http://s.init.im:8081": "/proxy.html"
+});
 
-React.render(
-    <div>Hello!</div>,
-    document.getElementById('content')
-);
+window.React = require('react');
+
+wyQuiz.setNewUUID = function() {
+
+  // If localStorage contains an existing UUID, use it as the UUID of the app.
+  // Otherwise, get a UUID from server.
+
+  "use strict";
+  if (localStorage.getItem('uuid')) {
+    this.uuid = localStorage.getItem('uuid');
+  } else {
+    var url = 'http://s.init.im:8081/utility/uuid/';
+    var uuid = 'DEFAULT'+Math.random().toString(); // In case UUID server fails
+    var request = new XMLHttpRequest();
+
+    request.open('GET', url, true);
+    request.onload = function () {
+      console.log('UUID server responded');
+      if (request.status >= 200 && request.status < 400) {
+        var response = JSON.parse(request.responseText);
+        if (response.success) {
+          uuid = response.data.uuid;
+        }
+      }
+      this.uuid = uuid;
+      localStorage.setItem('uuid', uuid);
+    }.bind(this);
+    request.send();
+  }
+};
+
+function post(keyToPost, valueToPost) {
+  "use strict";
+  var url = "http://s.init.im:8081/remember/wyIndexQuiz/";
+  var request = new XMLHttpRequest();
+  var message = {
+    username: wyQuiz.uuid,
+    key: keyToPost,
+    value: valueToPost,
+    raw: '',
+    datetime: Date.now()
+  };
+
+  request.open('POST', url, true);
+  request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+  var jsonString = JSON.stringify(message);
+  request.send(jsonString);
+  console.log('Tried to post '+jsonString);
+}
+
+function extend(object1, object2) {
+
+  // Parameters are not modified
+
+  var result = {},
+      prop;
+
+  for (prop in object1) if (object1.hasOwnProperty(prop)) {
+    result[prop] = object1[prop];
+  }
+
+  for (prop in object2) if (object2.hasOwnProperty(prop)) {
+    result[prop] = object2[prop];
+  }
+
+  return result;
+}
+
+var masterStyle = {
+  backgroundColor: '#2AB6C9',
+  fontFamily: 'sans-serif',
+  maxWidth: '1125px',
+  margin: '0 auto'
+};
+
+var QuestionCard = React.createClass({
+  render: function() {
+    return (
+        <div>Hello</div>
+    );
+  }
+});
+
+var CoverCard = React.createClass({
+
+  handleStartClick: function(event) {
+    target.preventDefault();
+    React.render(
+        <QuestionCard
+            survey={wyQuiz.survey}
+            serial={0}
+        />,
+        document.getElementById('content')
+    )
+  },
+
+  render: function() {
+    var cardStyle = {
+    };
+    cardStyle = extend(masterStyle, cardStyle);
+
+    var textBoxStyle = {
+      color: '#EEE',
+    };
+
+    var imageStyle = {
+      display: 'inline-block',
+      float: 'left',
+    };
+
+    var titleStyle = {
+      fontSize: '5em',
+      paddingTop: '1em'
+    };
+
+    var startTextStyle = {
+      fontColor: 'inherit',
+      fontSize: '2em',
+      textDecoration: 'none',
+      marginLeft: '15em'
+    };
+
+
+    return (
+        <div style={cardStyle}>
+          <img src={this.props.coverImagePath} style={imageStyle} />
+          <div id='textBox' style={textBoxStyle}>
+            <h1 style={titleStyle}>{this.props.title}</h1>
+            <a href=''
+               style={startTextStyle}
+               onClick={this.handleStartClick}>
+              {this.props.startText + "▶"}
+            </a>
+          </div>
+        </div>
+    )
+  }
+});
+
+(function(window, document, React, wyQuiz){
+  document.title = wyQuiz.title;
+  wyQuiz.setNewUUID();
+  React.render(
+      <CoverCard
+          title={wyQuiz.title}
+          startText={wyQuiz.startText}
+          coverImagePath={wyQuiz.coverImgRelativePath}
+      />,
+      document.getElementById('content')
+  );
+
+  post('render', wyQuiz.lang+'-rendered');
+}(window, window.document, window.React, window.wyQuiz));
+
+
